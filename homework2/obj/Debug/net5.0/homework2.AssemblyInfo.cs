45d146d2b761b756f6cbd44c6456d7e04c5458fd
unknown <achigognela@gmail.com>
2024-01-16 19:13:45 +0400

    homework2

--- homework2/obj/Debug/net5.0/homework2.AssemblyInfo.cs
+++ homework2/obj/Debug/net5.0/homework2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homework2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a204879d9179248b9852326d970a67b605cfc3b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("homework2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homework2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
